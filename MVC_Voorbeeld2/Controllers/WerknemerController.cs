421fbce6fadbb11cf3e7beabd27dccb7e7f7edb8
MichielAchten <michiel.achten@gmail.com>
2016-09-25 16:20:56 +0200

    Add project files.

---
+++ MVC_Voorbeeld2/Controllers/WerknemerController.cs
@@ -0,0 +1,31 @@
+﻿using MVC_Voorbeeld2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Voorbeeld2.Controllers
+{
+    public class WerknemerController : Controller
+    {
+        // GET: Werknemer
+        public ActionResult Index()
+        {
+            return View("AndereNaam");
+        }
+        [NonAction]
+        public void GeenAction()
+        {
+
+        }
+
+        public ActionResult AlleWerknemers()
+        {
+            List<Werknemer> werknemers = new List<Werknemer>();
+            werknemers.Add(new Werknemer { Voornaam = "Steven", Wedde = 1000m, InDienst = DateTime.Today });
+            werknemers.Add(new Werknemer { Voornaam = "steven", Wedde = 2000m, InDienst = DateTime.Today.AddDays(2) });
+            return View(werknemers);
+        }
+    }
+}
